54cb1b33c8754357a770af302f0c22b2921cdac5
lucas-andre <lucas.henrydz@gmail.com>
2023-01-12 22:43:40 -0300

    test(ratingartist): 100% coverage in ratingArtist usecase

--- src/reviews/usecases/ratingArtist.usecase.ts
+++ src/reviews/usecases/ratingArtist.usecase.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { QueryRunner } from 'typeorm/query-runner/QueryRunner';
 
 import { AgendaProvider } from '../../agenda/infrastructure/providers/agenda.provider';
 import { DomainUnProcessableEntity } from '../../global/domain/exceptions/domain.exception';
@@ -16,13 +15,7 @@ import {
   EVENT_NEEDS_TO_BE_DONE_TO_RATE,
   USER_IS_NOT_RELATED_TO_EVENT,
 } from '../../users/domain/errors/codes';
-import { USER_ALREADY_REVIEW_THE_EVENT } from '../codes';
-import { Review } from '../database/entities/review.entity';
-import {
-  defaultRatingMap,
-  RatingRate,
-  ReviewAvg,
-} from '../database/entities/reviewAvg.entity';
+import { ERROR_CREATING_REVIEW } from '../codes';
 import { ReviewProvider } from '../database/providers/review.provider';
 import { ReviewArtistRequestDto } from '../dtos/reviewArtistRequest.dto';
 
@@ -69,32 +62,26 @@ export class RatingArtistUsecase extends BaseUseCase implements UseCase {
         artistId,
       );
 
-    if (review && review.isRated) {
-      throw new DomainUnProcessableEntity(USER_ALREADY_REVIEW_THE_EVENT);
-    }
-
     let transactionIsOk = false;
 
-    // const reviewWasRatedBefore = await this.reviewProvider.hasReviews(eventId);
-
     if (!review) {
-      transactionIsOk = await this.createReviewTransact(
-        body,
+      transactionIsOk = await this.reviewProvider.createReviewTransaction(
         artistId,
         eventId,
         userId,
+        body,
       );
     } else {
-      transactionIsOk = await this.updateReviewTransact(
-        body,
+      transactionIsOk = await this.reviewProvider.updateReviewTransaction(
         artistId,
         eventId,
         userId,
+        body,
       );
     }
 
     if (!transactionIsOk) {
-      throw new DomainUnProcessableEntity('Error on rating artist');
+      throw new DomainUnProcessableEntity(ERROR_CREATING_REVIEW);
     }
 
     return {
@@ -103,207 +90,9 @@ export class RatingArtistUsecase extends BaseUseCase implements UseCase {
     };
   }
 
-  private async createReviewTransact(
-    body: ReviewArtistRequestDto,
-    artistId: number,
-    eventId: number,
-    userId: number,
-  ): Promise<boolean> {
-    let transactionIsOk = false;
-
-    const queryRunner = this.reviewProvider.source.createQueryRunner();
-
-    await queryRunner.startTransaction();
-
-    try {
-      await queryRunner.connect();
-
-      await queryRunner.manager
-        .createQueryBuilder()
-        .insert()
-        .into(Review)
-        .values({
-          value: body.rating,
-          artistId: artistId,
-          eventId: eventId,
-          createBy: userId,
-          displayName: body.displayName,
-          header: body.header,
-          content: body.comment,
-          isRated: true,
-        })
-        .execute();
-
-      await this.handleReviewAvg(queryRunner, artistId, body, eventId);
-
-      await queryRunner.commitTransaction();
-
-      transactionIsOk = true;
-    } catch (error) {
-      this.logger.error(error);
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
-    }
-
-    return transactionIsOk;
-  }
-
-  private async updateReviewTransact(
-    body: ReviewArtistRequestDto,
-    artistId: number,
-    eventId: number,
-    userId: number,
-  ): Promise<boolean> {
-    let transactionIsOk = false;
-    const queryRunner = this.reviewProvider.source.createQueryRunner();
-    await queryRunner.startTransaction();
-    try {
-      await queryRunner.connect();
-
-      await queryRunner.manager
-        .createQueryBuilder()
-        .update(Review)
-        .set({
-          content: body.comment,
-          value: body.rating,
-          isRated: true,
-        })
-        .where('createBy = :createBy', { createBy: userId })
-        .andWhere('artistId = :artistId', { artistId: artistId })
-        .andWhere('eventId = :eventId', { eventId: eventId })
-        .execute();
-
-      await this.handleReviewAvg(queryRunner, artistId, body, eventId);
-
-      await queryRunner.commitTransaction();
-
-      transactionIsOk = true;
-    } catch (error) {
-      this.logger.error(error);
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
-    }
-
-    return transactionIsOk;
-  }
-
-  private async handleReviewAvg(
-    queryRunner: QueryRunner,
-    artistId: number,
-    body: ReviewArtistRequestDto,
-    eventId: number,
-  ): Promise<void> {
-    const reviewAvg = await queryRunner.manager.findOne(ReviewAvg, {
-      where: {
-        artistId: artistId,
-        eventId: eventId,
-      },
-    });
-
-    if (!reviewAvg) {
-      await this.newReviewAvgTransact(queryRunner, body, artistId, eventId);
-    } else {
-      await this.updateReviewAvgTransact(
-        queryRunner,
-        body,
-        reviewAvg,
-        artistId,
-        eventId,
-      );
-    }
-  }
-
-  private async updateReviewAvgTransact(
-    queryRunner: QueryRunner,
-    body: ReviewArtistRequestDto,
-    reviewAvg: ReviewAvg,
-    artistId: number,
-    eventId: number,
-  ): Promise<void> {
-    const { newAvg, newCount, newDetail } = await this.getUpdateReviewAvgValues(
-      reviewAvg,
-      body.rating,
-    );
-
-    await queryRunner.manager
-      .createQueryBuilder()
-      .update(ReviewAvg)
-      .set({
-        value: newAvg,
-        count: newCount,
-        detail: newDetail,
-      })
-      .where('artistId = :artistId', { artistId: artistId })
-      .andWhere('eventId = :eventId', { eventId: eventId })
-      .execute();
-  }
-
-  private async newReviewAvgTransact(
-    queryRunner: QueryRunner,
-    body: ReviewArtistRequestDto,
-    artistId: number,
-    eventId: number,
-  ): Promise<void> {
-    const newRatingDetail = this.getNewRatingDetail(body);
-
-    await queryRunner.manager
-      .createQueryBuilder()
-      .insert()
-      .into(ReviewAvg)
-      .values({
-        artistId: artistId,
-        value: body.rating,
-        count: 1,
-        eventId: eventId,
-        detail: newRatingDetail,
-      })
-      .execute();
-  }
-
   private isUserNotReviewIt(body: ReviewArtistRequestDto): boolean {
     // We need to check if the user is not making a review and just leave a rating
     return !body.comment && !body.rating;
-  }
-
-  private computeNewAvg(
-    oldRatingAvg: number,
-    newCount: number,
-    newRating: number,
-  ): number {
-    // https://math.stackexchange.com/questions/106313/regular-average-calculated-accumulatively#:~:text=i.e.%20to%20calculate%20the%20new,divide%20the%20total%20by%20n.
-    return oldRatingAvg + (newRating - oldRatingAvg) / newCount;
-  }
-
-  private getNewRatingDetail(
-    body: ReviewArtistRequestDto,
-  ): Record<RatingRate, number> {
-    const newRatingDetail = defaultRatingMap;
-    newRatingDetail[body.rating] = newRatingDetail[body.rating] + 1;
-    return newRatingDetail;
-  }
-
-  private async getUpdateReviewAvgValues(
-    oldReviewAvg: ReviewAvg,
-    newRating: number,
-  ): Promise<{
-    newAvg: number;
-    newCount: number;
-    newDetail: Record<RatingRate, number>;
-  }> {
-    const { detail, value, count } = oldReviewAvg;
-
-    detail[newRating] = detail[newRating] + 1;
-
-    const newCount = count + 1;
-    const newAvg = this.computeNewAvg(value, newCount, newRating);
-
-    return {
-      newAvg,
-      newCount,
-      newDetail: detail,
-    };
   }
 
   private async emptyReviewFlow(
