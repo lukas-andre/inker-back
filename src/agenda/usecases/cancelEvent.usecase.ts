1675abb66fc57c9c2082d627098961a229c13d45
Lucas <noname@MacBook-Air-de-Lucas.local>
2024-05-25 16:37:54 -0400

    feat: add push job to queuees from usecases

--- src/agenda/usecases/cancelEvent.usecase.ts
+++ src/agenda/usecases/cancelEvent.usecase.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { Queue } from 'bull';
 import stringify from 'fast-safe-stringify';
 
 import {
@@ -9,17 +11,29 @@ import {
   BaseUseCase,
   UseCase,
 } from '../../global/domain/usecases/base.usecase';
+import { AgendaEventCanceledJobType } from '../../queues/notifications/domain/schemas/agenda';
+import { queues } from '../../queues/queues';
 import { Agenda } from '../infrastructure/entities/agenda.entity';
+import { AgendaEvent } from '../infrastructure/entities/agendaEvent.entity';
 import { AgendaProvider } from '../infrastructure/providers/agenda.provider';
 import { AgendaEventProvider } from '../infrastructure/providers/agendaEvent.provider';
 
 @Injectable()
-export class CancelEventUseCase extends BaseUseCase implements UseCase {
+export class CancelEventUseCase
+  extends BaseUseCase
+  implements UseCase, OnModuleDestroy
+{
   constructor(
     private readonly agendaProvider: AgendaProvider,
     private readonly agendaEventProvider: AgendaEventProvider,
+    @InjectQueue(queues.notification.name)
+    private readonly notificationQueue: Queue,
   ) {
     super(CancelEventUseCase.name);
+  }
+
+  async onModuleDestroy() {
+    await this.notificationQueue.close();
   }
 
   async execute(eventId: number, agendaId: number): Promise<Agenda> {
@@ -39,12 +53,39 @@ export class CancelEventUseCase extends BaseUseCase implements UseCase {
       const result = await this.agendaEventProvider.softDelete(eventId);
       this.logger.log(`Delete result: ${stringify(result)}`);
       if (result.affected) {
+        await this.pushAgendaEventCanceledEvent(event, existsAgenda);
         return existsAgenda;
       }
       throw new DomainInternalServerError('Fail when event is canceled');
     } catch (error) {
       this.logger.error(`Adding event error ${(error as Error).message}`);
       throw new DomainInternalServerError('Fail when event is canceled');
+    }
+  }
+
+  private async pushAgendaEventCanceledEvent(
+    event: AgendaEvent,
+    existsAgenda: Agenda,
+  ) {
+    const queueMessage: AgendaEventCanceledJobType = {
+      jobId: 'EVENT_CANCELED',
+      metadata: {
+        eventId: event.id,
+        artistId: existsAgenda.artistId,
+        customerId: event.customerId,
+      },
+      notificationTypeId: 'EMAIL',
+    };
+
+    try {
+      const job = await this.notificationQueue.add(queueMessage);
+      this.logger.log({
+        message: 'Event published to notification queue',
+        data: queueMessage,
+        job,
+      });
+    } catch (error) {
+      this.logger.error(error);
     }
   }
 }
