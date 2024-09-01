ebfe8739c00c7a6dd9ee5171d92078d3dc0d4592
Lucas <noname@MacBook-Air-de-Lucas.local>
2024-09-01 13:21:54 -0400

    add requests context service

---
+++ src/global/infrastructure/services/requestContext.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { ClsService } from 'nestjs-cls';
+
+import { UserType } from '../../../users/domain/enums/userType.enum';
+import {
+  JwtPayload,
+  JwtPermission,
+} from '../../domain/interfaces/jwtPayload.interface';
+import { InkerClsStore } from '../guards/auth.guard';
+
+@Injectable()
+export class RequestContextService {
+  constructor(private readonly cls: ClsService<InkerClsStore>) {}
+
+  get isArtist(): boolean {
+    return this.cls.get('jwt').userType === UserType.ARTIST;
+  }
+
+  get isNotArtist(): boolean {
+    return this.cls.get('jwt').userType !== UserType.ARTIST;
+  }
+
+  get isCustomer(): boolean {
+    return this.cls.get('jwt').userType === UserType.CUSTOMER;
+  }
+
+  get isNotCustomer(): boolean {
+    return this.cls.get('jwt').userType !== UserType.CUSTOMER;
+  }
+
+  get userId(): number {
+    return this.cls.get('jwt').id;
+  }
+
+  get userType(): UserType {
+    return this.cls.get('jwt').userType;
+  }
+
+  get userTypeId(): number {
+    return this.cls.get('jwt').userTypeId;
+  }
+
+  get jwtPayload(): JwtPayload {
+    return this.cls.get('jwt');
+  }
+
+  get permission(): JwtPermission[] {
+    return this.cls.get('jwt').permission;
+  }
+}
