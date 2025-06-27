fa55dd90a28f3feb2c16bac22926586eff7a756b
Lucas Henry <lucas.henrydz@gmail.com>
2025-06-26 23:16:58 -0400

    refactor: standardize use case import paths in locations module and handler

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -7,10 +7,10 @@ import { ReviewRepositoryModule } from '../reviews/database/reviewRepository.mod
 import { LocationRepositoryModule } from './infrastructure/database/locationRepository.module';
 import { LocationsController } from './infrastructure/locations.controller';
 import { LocationsHandler } from './infrastructure/locations.handler';
-import { CreateArtistLocationUseCase } from './useCases/artistLocations/createArtistLocation.usecase';
-import { DeleteArtistLocationUseCase } from './useCases/artistLocations/deleteArtistLocation.usecase';
-import { GetArtistLocationsUseCase } from './useCases/artistLocations/getArtistLocations.usecase';
-import { UpdateArtistLocationUseCase } from './useCases/artistLocations/updateArtistLocation.usecase';
+import { CreateArtistLocationUseCase } from './usecases/artistLocations/createArtistLocation.usecase';
+import { DeleteArtistLocationUseCase } from './usecases/artistLocations/deleteArtistLocation.usecase';
+import { GetArtistLocationsUseCase } from './usecases/artistLocations/getArtistLocations.usecase';
+import { UpdateArtistLocationUseCase } from './usecases/artistLocations/updateArtistLocation.usecase';
 import { AddLocationByApiUseCase } from './usecases/addLocationByApi.usecase';
 import { FindArtistByRangeUseCase } from './usecases/findArtistByRange.usecase';
 
